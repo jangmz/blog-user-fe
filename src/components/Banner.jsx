29268d721dd1cfe4d02af23fb0629e22ab8a01ef
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-16 11:42:22 +0100

    banner changes

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,17 @@
+import bannerImage from "../assets/banner-img.jpg"
+
 function Banner() {
     return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-6 justify-content-center">
-                    Welcome
+        <div id="banner" className="container-fluid d-flex align-items-center" style={{height: "500px"}}>
+            <div className="row w-100">
+                <div className="col-6 d-flex align-items-center">
+                    <h2>Welcome</h2>
                 </div>
-                <div className="col-6">
-                    Image
+                <div className="col-6 d-flex align-items-center p-0">
+                    <p>image</p>
+                    {
+                    //<img src={bannerImage} alt="Banner image" className="banner-image" />
+                    }
                 </div>
             </div>
         </div>
