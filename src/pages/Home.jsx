ec693a798068049166b2718e7e2b2f78d468835e
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-17 11:24:08 +0100

    page wrappers

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Banner from "../components/Banner";
+import BlogList from "../components/Blog/BlogList";
+
+export default function Home() {
+    return (
+        <>
+            <Banner />
+            <BlogList />
+        </>
+    )
+}
