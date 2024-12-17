d25df49c5a3ad32c1c650f4c031ee8e483486e0e
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-17 10:13:34 +0100

    basic error page

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+
     return (
         <div className="container d-flex flex-column justify-content-center align-items-center">
-            <h1>This route doesn't exist.</h1>
+            <h1>Error</h1>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
             <Link to="/">Go back to homepage</Link>
         </div>
     )
