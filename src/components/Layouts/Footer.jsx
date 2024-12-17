89286c3dfdbd2d2966d904a4a02d2bcb69583483
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-17 11:01:31 +0100

    changed dir

---
+++ src/components/Layouts/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer bg-secondary d-flex justify-content-center align-items-center mt-5" style={{ height: "60px"}} >
+            <p className="text-light m-0" >This project is part of learning from The Odin Project</p>
+        </footer>
+    )
+}
+
+export default Footer;
