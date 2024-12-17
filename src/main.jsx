0a57ba2c98e6c4c6ddb7ac0c81f8b6667e1a78fe
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-17 11:31:54 +0100

    routing and paths

--- src/main.jsx
+++ src/main.jsx
@@ -2,38 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Layout from "./components/Layouts/Layout.jsx";
+import Home from "./pages/Home.jsx";
+import PostDetails from './pages/PostDetails.jsx';
 import BlogList from './components/Blog/BlogList.jsx';
-import BlogArticle from './components/Blog/BlogArticle.jsx';
 import ErrorPage from './components/ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Layout />,
     errorElement: <ErrorPage />,
     children: [
-      {
-        path: "posts",
-        element: <BlogList />,
-        children: [
-          {
-            path:":postID",
-            element: <BlogArticle />
-          }
-        ]
-      }/*,{
-        path: "log-in",
-        element: 
-      }, {
-        path: sign-up",
-        element:
-      }, {
-        path:"log-out",
-        element:
-      }*/
+      { index: true, element: <Home /> }, // renders at "/"
+      { path: "posts", element: <BlogList /> },
+      { path: "posts/:postId", element: <PostDetails />},
+      /*{ path: "log-in", element: },
+      { path: "sign-up", element: },
+      { path: "log-out", element: }*/
     ]
   }
 ]);
