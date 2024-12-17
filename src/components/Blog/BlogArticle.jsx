fc37aa2521210b5bdc46e1d9b7365b777f48296e
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-17 14:16:42 +0100

    displaying article data

--- src/components/Blog/BlogArticle.jsx
+++ src/components/Blog/BlogArticle.jsx
@@ -1,7 +1,58 @@
-export default function BlogArticle() {
+import { useState } from "react";
+
+export default function BlogArticle({ id }) {
+    const [posts, setPosts] = useState([
+        {
+            id: 1,
+            title: "Test 1",
+            author: "testAuthor",
+            content: "Test content 1",
+            created: "12/12/2024"
+        },{
+            id:2,
+            title: "Test 2",
+            author: "testAuthor",
+            content: "Test content 2",
+            created: "12/12/2024"
+        },{
+            id: 3,
+            title: "Test 3",
+            author: "testAuthor",
+            content: "Test content 3",
+            created: "12/12/2024",
+            updated: "17/12/2024"
+        },{
+            id: 4,
+            title: "Test 4",
+            author: "testAuthor",
+            content: "Test content 4",
+            created: "12/12/2024"
+        },{
+            id: 5,
+            title: "Test 5",
+            author: "testAuthor",
+            content: "Test content 5",
+            created: "12/12/2024",
+            updated: "16/12/2024"
+        },{
+            id: 6,
+            title: "Test 6",
+            author: "testAuthor",
+            content: "Test content 6",
+            created: "12/12/2024"
+        }
+    ]);
+
+    const currentPost = posts.filter(post => post.id === parseInt(id));
+
     return (
         <div className="container">
-            <h1>Post title</h1>
+            <h1>{currentPost[0].title}</h1>
+            <h5>Author: {currentPost[0].author}</h5>
+            <h5>Posted: {currentPost[0].created}</h5>
+            {
+                currentPost[0].updated !== undefined ? <h5>Updated: {currentPost[0].updated}</h5> : ""
+            }
             <p>Post content.</p>
         </div>
     )
--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,10 +1,16 @@
+import { useParams } from "react-router-dom";
 import BlogArticle from "../components/Blog/BlogArticle";
 import CommentsSection from "../components/Blog/CommentsSection";
 
 export default function PostDetails() {
+    const params = useParams();
+    const id = params.postId;
+
     return (
         <div className="container-fluid d-flex flex-column align-items-center mt-4">
-            <BlogArticle />
+            <BlogArticle  
+                id={id}
+            />
             <CommentsSection />
         </div>
     )
