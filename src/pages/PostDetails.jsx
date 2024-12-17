35124b5355967ca5180ebccfff4eb44dad0ab4b1
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-17 11:48:00 +0100

    article page wrapper

---
+++ src/pages/PostDetails.jsx
@@ -0,0 +1,10 @@
+import BlogArticle from "../components/Blog/BlogArticle";
+
+export default function PostDetails() {
+    return (
+        <div className="container-fluid d-flex flex-column align-items-center mt-4">
+            <BlogArticle />
+            {/* add comment section here */}
+        </div>
+    )
+}
