aaff23e9b117fdc5f0accb8566805d8a91533962
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-17 11:09:09 +0100

    added routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,45 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import BlogAllArticles from './components/BlogAllArticles.jsx';
+import BlogArticle from './components/BlogArticle.jsx';
+import ErrorPage from './components/ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "posts",
+        element: <BlogAllArticles />,
+        /*children: [
+          {
+            path:":postID",
+            element: <BlogArticle />
+          }
+        ]*/
+      }/*,{
+        path: "log-in",
+        element: 
+      }, {
+        path: sign-up",
+        element:
+      }, {
+        path:"log-out",
+        element:
+      }*/
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
