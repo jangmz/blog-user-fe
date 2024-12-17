151814e9b43e427cd4ad5e2405c1e9f0447327bb
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-17 13:36:29 +0100

    post wrapper

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,10 +1,11 @@
 import BlogArticle from "../components/Blog/BlogArticle";
+import CommentsSection from "../components/Blog/CommentsSection";
 
 export default function PostDetails() {
     return (
         <div className="container-fluid d-flex flex-column align-items-center mt-4">
             <BlogArticle />
-            {/* add comment section here */}
+            <CommentsSection />
         </div>
     )
 }
