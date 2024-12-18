d4a97609f82caff39e2fbcd96dbcce1b55e44270
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-18 14:59:56 +0100

    handling data

--- src/components/Forms/FormInput.jsx
+++ src/components/Forms/FormInput.jsx
@@ -1,8 +1,8 @@
-export default function FormInput({ label, name, type, required }) {
+export default function FormInput({ label, name, type, required, onChange }) {
     return (
         <div className="mb-3">
             <label htmlFor={name} className="form-label">{label}</label>
-            <input type={type} name={name} id={name} className="form-control" required />
+            <input type={type} name={name} id={name} onChange={onChange} className="form-control" required />
         </div>
     )
 }
--- src/components/Forms/LogInForm.jsx
+++ src/components/Forms/LogInForm.jsx
@@ -1,20 +1,43 @@
+import { useState } from "react";
 import FormInput from "./FormInput";
 
 export default function LogInForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleUsername(e) {
+        setUsername(e.target.value);
+    }
+
+    function handlePassword(e) {
+        setPassword(e.target.value);
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(`Username: ${username}`);
+        console.log(`Password: ${password}`);
+
+        setPassword("");
+        setUsername("");
+    }
+
     return (
         <div className="container text-center">
             <h1 className="m-4">Log In</h1>
             <div className="container-fluid w-50">
-                <form action="http://localhost:5000/log-in" method="POST">
+                <form onSubmit={handleSubmit} action="http://localhost:5000/log-in" method="POST">
                     <FormInput
                         label={"Username"}
                         name={"username"}
                         type={"text"}
+                        onChange={e => handleUsername(e)}
                     />
                     <FormInput
                         label={"Password"}
                         name={"password"}
                         type={"password"}
+                        onChange={e => handlePassword(e)}
                     />
                     <button type="submit" className="btn btn-primary">Log In</button>
                 </form>
