bb41294125c918e16a2cd741a93e614549fff738
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-20 11:16:24 +0100

    lifted state for BlogArticle and CommentsSection component

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -1,17 +1,22 @@
 import { useParams } from "react-router-dom";
+import { useBlogContext } from "../context/BlogContext";
 import BlogArticle from "../components/Blog/BlogArticle";
 import CommentsSection from "../components/Blog/CommentsSection";
 
 export default function PostDetails() {
     const params = useParams();
     const id = params.postId;
+    const { posts } = useBlogContext()
+        const currentPost = posts.find(post => post.id === Number(id))
+    
+        if (!currentPost) {
+            throw new Error("Post not found.")
+        }
 
     return (
         <div className="container-fluid d-flex flex-column align-items-center mt-4">
-            <BlogArticle  
-                id={id}
-            />
-            <CommentsSection />
+            <BlogArticle currentPost={currentPost}/>
+            <CommentsSection currentPost={currentPost}/>
         </div>
     )
 }
