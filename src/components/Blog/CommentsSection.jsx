0dbe5c6c8f2cfabd181fd456346c6ec1890058d4
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-20 11:17:35 +0100

    display comments from the post

--- src/components/Blog/CommentsSection.jsx
+++ src/components/Blog/CommentsSection.jsx
@@ -1,36 +1,23 @@
-import { useState } from "react"
-import Comment from "./Comment";
+import Comment from "./Comment"
 
-/* TODO: for fetched comments from DB, change userId to username (user) */
-
-export default function CommentsSection() {
-    const [comments, setComments] = useState([
-        {
-            id: 1,
-            content: "This comment is a test.",
-            created: "12/12/2024",
-            user: "admin" 
-        },{
-            id: 2,
-            content: "This comment is a test.",
-            created: "23/12/2024",
-            user: "testUser"
-        }
-    ]);
+export default function CommentsSection({ currentPost }) {
+    const comments = currentPost.comments
 
     return (
         <div className="container justify-content-center">
             <h3>Comments</h3>
             {
+                comments.length > 0 ? 
                 comments.map(comment => (
                     <Comment 
                         key={comment.id}
                         id={comment.id}
                         content={comment.content}
                         created={comment.created}
-                        user={comment.user}
+                        userId={comment.userId}
                     />
-                ))
+                )) :
+                <p>There are no comments</p>
             }
         </div>
     )
