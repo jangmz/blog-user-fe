c4e97c6a7b863fbc56cfe6ccd833fd0d9e60baea
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-20 14:13:09 +0100

    fetching user log in data

--- src/components/Forms/LogInForm.jsx
+++ src/components/Forms/LogInForm.jsx
@@ -2,31 +2,60 @@ import { useState } from "react";
 import FormInput from "./FormInput";
 
 export default function LogInForm() {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+    const [userLogIn, setUserLogIn] = useState({
+        username: "",
+        password: "",
+    })
+    const [error, setError] = useState(null)
 
     function handleUsername(e) {
-        setUsername(e.target.value);
+        setUserLogIn({...userLogIn, username: e.target.value})
     }
 
     function handlePassword(e) {
-        setPassword(e.target.value);
+        setUserLogIn({...userLogIn, password: e.target.value})
     }
 
-    function handleSubmit(e){
-        e.preventDefault();
-        console.log(`Username: ${username}`);
-        console.log(`Password: ${password}`);
+    async function handleSubmit(e){
+        e.preventDefault()
+        console.log("User data input: ", JSON.stringify(userLogIn))
 
-        setPassword("");
-        setUsername("");
+        fetch("http://localhost:5000/log-in", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(userLogIn)
+        })
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(errorData => {
+                        throw new Error(errorData.error || "Unknown error occured.")
+                    })
+                }
+                return response.json()
+            })
+            .then(data => {
+                // save tokens to localStorage
+                console.log(data)
+            })
+            .catch(error => {
+                console.log("Error during log in: ", error)
+                setError(error.message)
+            })
     }
 
     return (
         <div className="container text-center">
             <h1 className="m-4">Log In</h1>
+            {
+                error &&
+                <div className="alert alert-danger" role="alert">
+                    {error}
+                </div> 
+            }
             <div className="container-fluid w-50">
-                <form onSubmit={handleSubmit} action="http://localhost:5000/log-in" method="POST">
+                <form onSubmit={handleSubmit}>
                     <FormInput
                         label={"Username"}
                         name={"username"}
