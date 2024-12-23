bc9f5f5fcee3a772db447aeff657369eefe7613c
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-23 15:12:31 +0100

    fix is needed on displaying different navbars

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -1,8 +1,23 @@
 import { Link } from "react-router-dom";
 import UserNavbar from "./UserNavbar";
 import GeneralNavbar from "./GeneralNavbar";
+import { jwtDecode } from "jwt-decode";
+import { useAuth } from "../../context/AuthContext";
 
 function Navbar() {
+    const { token } = useAuth();
+    let username = null;
+
+    if (token) {
+        try {
+            const decodedToken = jwtDecode(token);
+            console.log(decodedToken);
+            username = decodedToken.username; // Adjust based on your token structure
+        } catch (error) {
+            console.error("Invalid token:", error);
+        }
+    }
+
     return (
         <nav id="navbar" className="navbar navbar-expand-md bg-primary">
             <div className="container-fluid">
@@ -13,11 +28,11 @@ function Navbar() {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-                    {
-                        localStorage.getItem("refreshToken") ?
-                            <UserNavbar /> : <GeneralNavbar />
-                            //TODO: implement AuthorNavbar
-                    }
+                    <GeneralNavbar />
+                    {/*
+                        TODO: different navbars for different users need a re-render?
+                        username ? <UserNavbar /> : <GeneralNavbar />
+                    */}
                 </div>
                 
             </div>
