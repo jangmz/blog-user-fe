892db2c75228994058b360beaccaa5121b49a27c
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-23 15:13:40 +0100

    auth provider implemented but will probably be removed

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import LogInForm from './components/Forms/LogInForm.jsx';
 import SignUpForm from './components/Forms/SignUpForm.jsx';
 import { BlogProvider } from './context/BlogContext.jsx';
 import LogOut from './components/LogOut.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
 
 const router = createBrowserRouter([
   {
@@ -31,7 +32,10 @@ const router = createBrowserRouter([
 ]);
 
 createRoot(document.getElementById('root')).render(
+  <AuthProvider>
     <BlogProvider>
       <RouterProvider router={router} />
     </BlogProvider>
+  </AuthProvider>
+    
 )
