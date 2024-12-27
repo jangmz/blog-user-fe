a6bb8ca7b7bb04a6024c3591e6177564a7f139b2
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-27 18:13:51 +0100

    deleted jwt decode

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -1,21 +1,10 @@
 import { Link } from "react-router-dom";
 import UserNavbar from "./UserNavbar";
 import GeneralNavbar from "./GeneralNavbar";
-import { jwtDecode } from "jwt-decode";
 import { useAuth } from "../../context/AuthContext";
 
 function Navbar() {
     const { user } = useAuth();
-
-    if (token) {
-        try {
-            const decodedToken = jwtDecode(token);
-            console.log(decodedToken); // user object
-            username = decodedToken.username; // Adjust based on your token structure
-        } catch (error) {
-            console.error("Invalid token:", error);
-        }
-    }
 
     return (
         <nav id="navbar" className="navbar navbar-expand-md bg-primary">
