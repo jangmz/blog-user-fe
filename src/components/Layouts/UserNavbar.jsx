f75de9ecb6c1339ee800da7b1c2f0a01341c734f
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-28 11:19:37 +0100

    added link to user profile

--- src/components/Layouts/UserNavbar.jsx
+++ src/components/Layouts/UserNavbar.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 export default function UserNavbar() {
+    const auth = useAuth()
+
     return(
         <ul className="navbar-nav ms-auto d-flex gap-2">
             <li className="nav-item">
@@ -10,7 +13,11 @@ export default function UserNavbar() {
                 <Link className="nav-link" to="posts">Blog</Link>
             </li>
             <li className="nav-item">
-                <Link to="log-out" className="nav-link">Log Out</Link>
+                <Link className="nav-link" to="profile">Profile(not available)</Link>
+            </li>
+            <li className="nav-item">
+                <button className="nav-link" onClick={auth.logOut}>Log Out</button>
+
             </li>
         </ul>
     )
