97663ea5caf74dec0bd33043506810157749ac6f
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-28 11:16:54 +0100

    log in and logout functionality fixed

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,24 +3,23 @@ import { useState, createContext, useContext } from "react";
 
 const AuthContext = createContext()
 
-export function AuthProvider({ children }) {
-    const [token, setToken] = useState(localStorage.getItem("refreshToken"))
-    const [user, setUser] = useState(null) 
+function AuthProvider({ children }) {
+    const [token, setToken] = useState(localStorage.getItem("refreshToken") || "")
+    const [user, setUser] = useState()
 
-    async function logIn(userData) { // username, password
+    async function logIn(userData) { // input -> username, password
         console.log("Logging user in...")
         
         fetch("http://localhost:5000/log-in", {
             method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(userData)
         })
             .then(response => {
                 if (!response.ok) {
                     return response.json().then(errorData => {
-                        throw new Error(errorData.error || "Unknown error occured.")
+                        console.log("Details of error from server: ", errorData.error)
+                        throw new Error(errorData.error)
                     })
                 }
                 return response.json()
@@ -33,13 +32,15 @@ export function AuthProvider({ children }) {
                 localStorage.setItem("refreshToken", data.refreshToken)
                 
                 const { username, role } = jwtDecode(data.refreshToken)
+
+                console.log(`User data for context. Username: ${username}, Role: ${role}`)
                 
                 setToken(data.refreshToken) // token assigned
                 setUser({ username, role })
             })
             .catch(error => {
-                console.log("Error during log in: ", error)
-                throw new Error(error.message) // setError in component
+                console.log(error.message)
+                alert(error.message)
             })
     }
 
@@ -62,7 +63,7 @@ export function AuthProvider({ children }) {
             localStorage.removeItem("refreshToken")
             localStorage.removeItem("accessToken")
 
-            setToken(null)
+            setToken("")
             setUser(null)
 
             console.log("User logged out.")
@@ -78,6 +79,8 @@ export function AuthProvider({ children }) {
         </AuthContext.Provider>
     )
 }
+
+export default AuthProvider
 
 export function useAuth() {
     return useContext(AuthContext)
