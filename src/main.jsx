c4850b99a8cabb2c2cb783c142560011e9f0c712
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-28 20:16:12 +0100

    added route for profile

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import LogInForm from './components/Forms/LogInForm.jsx';
 import SignUpForm from './components/Forms/SignUpForm.jsx';
 import { BlogProvider } from './context/BlogContext.jsx';
 import AuthProvider from './context/AuthContext.jsx';
+import UserProfile from './components/UserProfile.jsx';
 
 const router = createBrowserRouter([
   {
@@ -22,9 +23,10 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <Home /> }, // renders at "/"
       { path: "posts", element: <Posts /> },
-      { path: "posts/:postId", element: <PostDetails />},
-      { path: "log-in", element: <LogInForm />},
-      { path: "sign-up", element: <SignUpForm />},
+      { path: "posts/:postId", element: <PostDetails /> },
+      { path: "log-in", element: <LogInForm /> },
+      { path: "sign-up", element: <SignUpForm /> },
+      { path: "profile", element: <UserProfile /> },
     ]
   }
 ]);
