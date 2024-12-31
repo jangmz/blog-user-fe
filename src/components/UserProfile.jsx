c4212d2e8ded12eda08cd22f85b1a177e1250a2b
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-31 16:06:43 +0100

    created my profile form for updating/deleting data

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,43 +1,63 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import FormInput from "./Forms/FormInput";
+import DropdownSelection from "./Forms/DropdownSelection";
 
 function UserProfile() {
-    const [user, setUser] = useState(useAuth.user || null) // useAuth.user is null!!!
-    const navigate = useNavigate()
-
-    if (!user) {
-        navigate("/log-in")
-    }
+    const {user} = useAuth()
+    const [userData, setUserData] = useState(user || null)
+    
+    /* TODO #1: If user isn't logged in redirect the user to log in screen */
+    /* TODO #2: Update new data in DB, context, localStorage */
+    /* TODO #3: Delete user */
 
     function handleInput(e) {
         const { name, value } = e.target
-        setUser((prev) => ({...prev, [name]: value}))
+        setUserData((prev) => ({...prev, [name]: value}))
     }
 
     async function handleUpdate(e) {
         e.preventDefault()
+        console.log("New user data: ", userData)
+    }
 
-        // update new data in DB, context, localstorage
-        console.log("New user data: ", user)
+    function deleteAccount() {
+        console.log("User will be deleted: ", user)
     }
 
     return (
         <div className="container">
-            <h1 className="text-center">My profile</h1>
-            <form onSubmit={handleUpdate}>
-                {/* TODO: use FormInput components & call handleInput() */}
-                <label htmlFor="username">Username</label>
-                <input type="text" name="username" value={user.username}/>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" value={user.email} />
-                <label htmlFor="role">Role</label>
-                <select name="role" id="role" value={user.role}>
-                    <option value="USER">User</option>
-                    <option value="AUTHOR">Author</option>
-                </select>
-                {/* TODO: add option to delete account */}
-            </form>
+            <h1 className="text-center m-4">My profile</h1>
+            <div className="container-fluid w-50">
+                <form onSubmit={handleUpdate}>
+                    <FormInput 
+                        label={"Username"}
+                        type={"text"}
+                        name={"username"}
+                        value={userData.username}
+                        onChange={e => handleInput(e)}
+                    />
+                    <FormInput 
+                        label={"E-mail"}
+                        type={"email"}
+                        name={"email"}
+                        value={userData.email}
+                        onChange={e => handleInput(e)}
+                    />
+                    <DropdownSelection 
+                        label={"Role"}
+                        name={"role"}
+                        selectValue={userData.role}
+                        values={["USER", "AUTHOR"]}
+                        onChange={e => handleInput(e)}
+                    />
+                    <div className="d-grid mt-3 gap-3">
+                        <button className="btn btn-primary">Update data</button>
+                        <button className="btn btn-danger" onClick={deleteAccount}>Delete account</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
