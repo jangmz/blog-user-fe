e0352419e4fc7a12e58ad25528895fcfd956cf87
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-31 15:35:41 +0100

    added value prop

--- src/components/Forms/FormInput.jsx
+++ src/components/Forms/FormInput.jsx
@@ -1,8 +1,8 @@
-export default function FormInput({ label, name, type, required, onChange }) {
+export default function FormInput({ label, name, type, value, required, onChange }) {
     return (
         <div className="mb-3">
             <label htmlFor={name} className="form-label">{label}</label>
-            <input type={type} name={name} id={name} onChange={onChange} className="form-control" required />
+            <input type={type} name={name} id={name} value={value} onChange={onChange} className="form-control" required />
         </div>
     )
 }
