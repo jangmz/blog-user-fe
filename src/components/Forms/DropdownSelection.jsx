c07d0e4ef075beb6428357deb2d237952ef65523
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-03 13:32:09 +0100

    added on change method

--- src/components/Forms/DropdownSelection.jsx
+++ src/components/Forms/DropdownSelection.jsx
@@ -1,8 +1,8 @@
-export default function DropdownSelection({ label, name, selectValue, values }) {
+export default function DropdownSelection({ label, name, selectValue, values, onChange }) {
     return (
         <div className="mb-3">
             <label htmlFor={name} className="form-label">{label}</label>
-            <select name={name} id={name} value={selectValue} className="form-select">
+            <select name={name} id={name} value={selectValue} onChange={onChange} className="form-select">
                 {
                     values.map(value => (
                         <option key={value} value={value}>{value}</option>
