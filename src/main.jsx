c533dc9da690de8f7f56cb45c6311b6271c49a6f
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-06 14:15:32 +0100

    path to dashboard

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import SignUpForm from './components/Forms/SignUpForm.jsx';
 import { BlogProvider } from './context/BlogContext.jsx';
 import AuthProvider from './context/AuthContext.jsx';
 import UserProfile from './components/UserProfile.jsx';
+import AuthorNavbar from './components/Layouts/AuthorNavbar.jsx';
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,7 @@ const router = createBrowserRouter([
       { path: "log-in", element: <LogInForm /> },
       { path: "sign-up", element: <SignUpForm /> },
       { path: "profile", element: <UserProfile /> },
+      { path: "dashboard", element: <AuthorNavbar /> },
     ]
   }
 ]);
