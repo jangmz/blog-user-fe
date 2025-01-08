e08f8f36df15b623a8c80e7d3a508326458f656f
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-08 10:56:34 +0100

    updated banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,19 +1,16 @@
-import bannerImage from "../assets/banner-img.jpg"
+import bannerImage from "../assets/banner-img2.jpg"
 
 function Banner() {
     return (
-        <div id="banner" className="container-fluid d-flex align-items-center bg-light" style={{height: "500px"}}>
-            <div className="row w-100">
+        <div id="banner" className="container-fluid d-flex align-items-center bg-light p-0" style={{height: "auto", overflow:"hidden"}}>
+            <div className="row w-100 m-0">
                 <div className="col-6 d-flex flex-column justify-content-center align-items-center">
                     <h1>Welcome</h1>
                     <h5>TO BEST TECH BLOG</h5>
                     <i>by Jan</i>
                 </div>
-                <div className="col-6 d-flex justify-content-center align-items-center p-0">
-                    <p>image</p>
-                    {
-                    //<img src={bannerImage} alt="Banner image" className="banner-image" />
-                    }
+                <div className="col-6 d-flex justify-content-end align-items-center p-0">
+                    <img src={bannerImage} alt="Banner image" className="banner-image w-100 h-auto" />
                 </div>
             </div>
         </div>
