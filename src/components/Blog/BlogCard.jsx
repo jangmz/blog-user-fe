64ca3063becbac7b76a9cfc7a53bc1f9171baeeb
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-10 11:15:54 +0100

    random generated thumbnail image

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -5,7 +5,7 @@ function BlogCard({ id, title, content, created }) {
 
     return (
         <div className="card p-0 m-3 col-xs-11 col-md-5 col-lg-3 col-xl-3">
-            <img src="..." className="card-img-top" alt="..."/>
+            <img src="https://picsum.photos/800/400" className="card-img-top" alt="Random image" />
             <div className="card-body">
                 <h5 className="card-title">
                     {title}
