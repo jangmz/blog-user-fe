4ba388bdc4a6d730be7cded37d39895c7a1de551
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-13 12:56:30 +0100

    note while loading posts

--- src/components/Blog/BlogList.jsx
+++ src/components/Blog/BlogList.jsx
@@ -9,12 +9,17 @@ export default function BlogList() {
             <div className="row w-100 justify-content-center">
                 {
                     loading &&
-                    <div className="d-flex justify-content-center">
-                        <div className="spinner-border" role="status">
+                    <div className="d-flex flex-column align-items-center justify-content-center">
+                        <div className="spinner-border m-3" role="status">
                             <span className="visually-hidden">Loading...</span>
                         </div>
                         <div className="alert alert-info text-center" role="alert">
-                            Please wait about 1 min for backend to start up as it is hosted on a service that puts application to sleep during periods of inactivity.
+                            <p className="m-0">
+                                Please wait about 1 min for backend to start up as it is hosted on a service that puts application to sleep during periods of inactivity.
+                            </p>
+                            <p className="m-0">
+                                After posts are loaded, everything will work normally.
+                            </p>
                         </div>
                     </div>
                 }
